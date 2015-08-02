d3ebc99b614aa0489e58b04341a0697a8833a681
Christoph Dorn <christoph@christophdorn.com>
2015-08-02 00:06:39 -0700

    wip

--- templates/nodejs/server.js
+++ templates/nodejs/server.js
@@ -1,12 +1,14 @@
 
 var http = require('http');
 
+const PORT = (process.env.PORT || 5000);
+
 http.createServer(function (req, res) {
 
   res.writeHead(200, {'Content-Type': 'text/plain'});
 
   res.end('Hello World from pinf sample app\n');
 
-}).listen((process.env.PORT || 5000), '127.0.0.1');
+}).listen(PORT);
 
 console.log('Server running at http://127.0.0.1:1337/');
